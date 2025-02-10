eae7b0b47d1658081a64078b5b20b8c4a5610969
Alexis <alexisgbr.contact@gmail.com>
2025-02-10 13:40:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import streamlit as st
 import os
 import sys
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+# print the current working directory
+print(os.getcwd())
 
 from pages.ressources.components import Navbar , display_quiz 
 from src.db.utils import QuizDatabase ,CoursesDatabase
