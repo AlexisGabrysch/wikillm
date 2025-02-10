af06744eda5c51cee9116ae531db75a8ccf80997
Alexis <alexisgbr.contact@gmail.com>
2025-02-10 14:11:54 +0100

    Update components.py

--- pages/ressources/components.py
+++ pages/ressources/components.py
@@ -1,15 +1,4 @@
 import streamlit as st
-import os
-
-# print the current working directory
-print(os.getcwd())
-import os
-import sys
-# print the current working directory
-print(os.getcwd())
-sys.path.append(os.path.abspath(os.path.dirname(__file__)))  # Add current dir
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))  # Add parent dir
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))  # Add parent dir
 
 from src.metrics_database import RAGMetricsDatabase
 import time
